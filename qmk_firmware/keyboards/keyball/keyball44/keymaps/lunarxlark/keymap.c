272775c8683f931a08e8793e135b97e4d0b10d93
yuta kobayashi <lunarxlark@gmail.com>
2024-04-22 23:06:23 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/lunarxlark/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/lunarxlark/keymap.c
@@ -24,14 +24,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E    , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E    , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D    , KC_F     , KC_G     ,                                         KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C    , KC_V     , KC_B     ,                                         KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
                   KC_LALT  , KC_LNG2 , KC_LGUI  ,         MO(1)    , KC_SPC   ,                    KC_ENT  , MO(2)    , _______       , _______  , KC_LNG1
   ),
 
   [1] = LAYOUT_universal(
-    KC_TAB   , _______  , _______  , _______ , _______  , _______  ,                                         _______  , _______  , _______  , _______  , _______  , KC_BSPC  ,
+    KC_ESC   , _______  , _______  , _______ , _______  , _______  ,                                         _______  , _______  , _______  , _______  , _______  , KC_BSPC  ,
     KC_LCTL  , S(KC_1)  , S(KC_2)  , S(KC_3) , S(KC_4)  , S(KC_5)  ,                                         S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , KC_F12   ,
     KC_LSFT  , KC_1     , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_RSFT  ,
                   KC_LALT  , _______ , KC_LGUI  ,         _______  , SCRL_TO  ,                   _______  , MO(3)    , _______       , _______  , _______
