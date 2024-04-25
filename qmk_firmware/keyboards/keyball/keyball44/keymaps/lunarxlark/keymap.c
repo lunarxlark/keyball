4042b3ffd492465d09322f9fecfb5fafa6e9121c
yuta kobayashi <lunarxlark@gmail.com>
2024-04-25 13:36:58 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/lunarxlark/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/lunarxlark/keymap.c
@@ -27,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E    , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D    , KC_F     , KC_G     ,                                         KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C    , KC_V     , KC_B     ,                                         KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-                  KC_LALT  , KC_LNG2 , KC_LGUI  ,         TO(1)    , KC_SPC   ,                    KC_ENT  , TO(2)    , _______       , _______  , KC_LNG1
+                  KC_LALT  , KC_LNG2 , KC_LGUI  ,         MO(1)    , KC_SPC   ,                    KC_ENT  , MO(2)    , _______       , _______  , KC_LNG1
   ),
 
   [1] = LAYOUT_universal(
     KC_ESC   , _______  , _______  , _______ , _______  , _______  ,                                         _______  , _______  , _______  , _______  , _______  , KC_BSPC  ,
     KC_LCTL  , S(KC_1)  , S(KC_2)  , S(KC_3) , S(KC_4)  , S(KC_5)  ,                                         S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , KC_BSLS  ,
     KC_LSFT  , KC_1     , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_RSFT  ,
-                  KC_LALT  , _______ , KC_LGUI  ,         _______  , SCRL_MO  ,                   _______  , TO(3)    , _______       , _______  , _______
+                  KC_LALT  , _______ , KC_LGUI  ,         _______  , SCRL_MO  ,                   _______  , MO(3)    , _______       , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
     _______  , KC_F11   , KC_F12   , KC_F13  , KC_F14   , KC_F15   ,                                         KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , KC_LBRC  , KC_RBRC  ,
     KC_LCTL  , KC_F6    , KC_F7    , KC_F8   , KC_F9    , KC_F10   ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_MINS  , KC_EQL   ,
     KC_LSFT  , KC_F1    , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_BTN4  , KC_BTN1  , KC_BTN2  , KC_BTN5  , _______ , KC_GRV    ,
-                  KC_LALT  , _______ , KC_LGUI  ,         TO(3)    , _______  ,                   _______  , _______  , _______       , _______  , _______
+                  KC_LALT  , _______ , KC_LGUI  ,         MO(3)    , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
